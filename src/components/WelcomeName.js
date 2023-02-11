da263cb63b29cc22f2e5bfb25a58f36db263b6f5
layadang <pd03@bu.edu>
2023-02-11 12:11:03 -0500

    Used React to display welcome name

--- src/components/WelcomeName.js
+++ src/components/WelcomeName.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const WelcomeName = ({ name }) => {
+const handleSubmit = (event, name) => {
+  alert('Your favorite flavor is: ' + name);
+  event.preventDefault();
+}
+
+function WelcomeName(props) {
+  const [name, setName] = useState(props.name);
 
     return (
       <div>
-        {name ? <h1>Welcome, {name}!</h1> : <h1>Please enter your name</h1>}  
+        {name ? <h1>Welcome, {name}!</h1> : <h1>Please enter your name</h1>}
+        <form onSubmit={e => handleSubmit(e, name)}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={e => {
+            setName(e.target.value)
+          } }/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
     );
   };
