dbe4acb9bd1fee00a9e9917aa87c456ff266f98a
achen32 <122934600+achen32@users.noreply.github.com>
2023-02-11 20:30:46 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import './App.css';
-import WelcomeName from './components/WelcomeName';
-import Wheel from './components/Wheel';
+import React from "react";
+import GetUserInfo from "./components/GetUserInfo";
+import { useState, useEffect } from "react";
+import Aztro from "./components/Aztro";
+import GetAstrologicalSign from "./components/GetAstrologicalSign.js";
 
-function App() {
+const App = () => {
+  const [birthday, setBirthday] = useState("");
+  const [astrologicalSign, setAstrologicalSign] = useState("");
+  const [sign, setSign] = useState("");
+
+
+  useEffect(() => {
+    const storedBirthday = localStorage.getItem("birthday") || "";
+    setBirthday(storedBirthday);
+    console.log(storedBirthday);
+    if (storedBirthday) {
+      setAstrologicalSign(GetAstrologicalSign(storedBirthday));
+    }
+    const storedSign = localStorage.getItem("sign");
+    console.log(localStorage.getItem("sign"));
+
+   }, []);
+
   return (
-    <div className="parent">
-
-      <div className="components">  
-        <WelcomeName name={''}/> 
-        <Wheel/>
-
-      </div>
+    <div>
+      <GetUserInfo />
+      <GetAstrologicalSign birthday={localStorage.getItem("birthday")} />
 
     </div>
   );
-}
+};
 
 export default App;
-
